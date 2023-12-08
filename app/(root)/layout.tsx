0f402eb28434edce0ec6daf96b970721fed5b0ac
Sebastian <sebastianfelenczak@gmail.com>
2023-12-08 13:34:14 +0100

    style: root page layout

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,21 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <main className="flex flex-col justify-center border-4 mx-[300px]">
+        <nav className="flex justify-center w-full border border-blue-400">
+          NAVBAR
+        </nav>
+        <article className="flex justify-center border border-red-500 h-screen">
+          <section className="border w-[350px] flex justify-center">
+            LEFT SIDE
+          </section>
+          <article className="w-full flex justify-center">{children}</article>
+          <section className="border w-[350px] flex justify-center">
+            RIGHT SIDE
+          </section>
+        </article>
+        <footer className="w-full flex justify-center">FOOTER</footer>
+      </main>
+    </>
+  );
+}
